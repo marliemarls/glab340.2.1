a36904c549fcf7348c10e182bc5562de65f32cb4
Marlie Martinez <marlieahue@gmail.com>
2024-10-07 11:01:29 -0400

    first commit

---
+++ AssignmentOperators.py
@@ -0,0 +1,20 @@
+a = 32
+b = 6
+print('a=b', a==b)
+print('a+=b', a+b)
+print('a-=b', a-b)
+print('a*=b', a*b)
+print('a%=b', a%b)
+print('a**=b', a**b)
+print('a//=b', a//b)
+
+j = 10 
+print("j is: ", j)
+j = 5
+k = j
+print("j is: ", j)
+print("k is: ", k)
+
+k = j = 10;
+print("j is : ", j)
+print("k is : ", k)
---
+++ ComparisonOperators.py
@@ -0,0 +1,8 @@
+a = 32      # Initialize the value of a  
+b = 6       # Initialize the value of b  
+print('Two numbers are equal or not:',a==b)  
+print('Two numbers are not equal or not:',a!=b)  
+print('a is less than or equal to b:',a<=b)  
+print('a is greater than or equal to b:',a>=b)  
+print('a is greater b:',a>b)  
+print('a is less than b:',a<b)  
